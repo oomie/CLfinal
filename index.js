95b0db3f2662e98f985f6e225e79e717abffa62d
oomie <fma9304@nyu.edu>
2022-12-12 11:12:05 +0400

    updated files

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-app.use('/', express.static('public'));
+app.use("/", express.static("public"));
 
-let http = require('http');
+let http = require("http");
 let server = http.createServer(app);
 let port = process.env.PORT || 4000;
 server.listen(port, () => {
-    console.log("Server listening at port " + port);
+  console.log("Server listening at port " + port);
 });
 
-
-// let io = require('socket.io');
-// io = new io.Server(server);
-
-// io.sockets.on('connection', function(socket) {
-//     console.log("We have a new client: " + socket.id);
-
-//     // socket.on('newUser', (data) => {
-//     //     userProperties = { id: socket.id, name: data.name };
-//     //     socketNames.push(userProperties.name); // stores name in the array
-//     //     socket.broadcast.emit('newUser', userProperties);
-//     // });
-
-//     //Listen for this client to disconnect
-//     socket.on('disconnect', function() {
-//         console.log("A client has disconnected: " + socket.id);
-//     });
-// });
-
 const { Database } = require("quickmongo");
-const db = new Database("mongodb+srv://toomie:00M1EZ00M1E.@clfinal.of9oacx.mongodb.net/?retryWrites=true&w=majority");
+const db = new Database(
+  "mongodb+srv://toomie:00M1EZ00M1E.@clfinal.of9oacx.mongodb.net/?retryWrites=true&w=majority"
+);
 
 db.on("ready", () => {
-    console.log("Connected to the database");
+  console.log("Connected to the database");
 });
 
 db.connect();
@@ -43,39 +26,26 @@ let plants = [];
 
 //2. add a route on server, that is listening for a post request
 
-app.post('/plantdata', (req, res) => { //this is posting whenever a new object is made 
-    console.log(req.body);
-    let currentDate = Date();
-    let plant = {
-        plant: req.body,
-        date: currentDate
-    }
+app.post("/plantdata", (req, res) => {
+  //this is posting whenever a new object is made
+  console.log(req.body);
+  let currentDate = Date();
+  let plant = {
+    plant: req.body,
+    date: currentDate,
+  };
 
-    //DB - 2 - add values to the DB
-    db.set("plantdata", req.body);
-    res.json({ task: "success" });
-})
+  //DB - 2 - add values to the DB
+  db.set("plantdata", req.body);
+  res.json({ task: "success" });
+});
 
 // //add route to get all plant information
-app.get('/getplants', (req, res) => {
-    //DB - 3 - fetch from the DB
-    db.get("plantdata").then(plantdata => {
-        let plant = { data: plantdata };
-        console.log(plantdata);
-        res.json(plant);
-    
-    })
-
-})
-
-
-
-// app.get('/getplants', (req,res)=> {
-//     //DB - 3 - fetch from the DB
-//     db.get("plants").then(getplantdata => {
-//         let plantObj = {data: getplantdata};
-//         res.json(plantObj);
-//         console.log(plantObj);
-//     })
-// }) //hi
-
+app.get("/getplants", (req, res) => {
+  //DB - 3 - fetch from the DB
+  db.get("plantdata").then((plantdata) => {
+    let plant = { data: plantdata };
+    console.log(plantdata);
+    res.json(plant);
+  });
+});
